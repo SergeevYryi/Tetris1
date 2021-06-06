15020b6c14228d178b42223b107f30cb84bceb4f
SergeevYryi <sergeev.y90@inbox.ru>
2021-06-06 18:16:49 +0400

    Game Tetris

---
+++ src/main/java/userInt/Config.java
@@ -0,0 +1,15 @@
+package userInt;
+
+import java.awt.*;
+
+public class Config {
+    public static final int WIDTH = 10;
+    public static final int HEIGHT = 20;
+
+    public static final int SIZE = 20;
+
+    public static final Color [] COLORS = {
+            Color.GRAY,
+            Color.BLUE,
+            Color.GREEN};
+}
